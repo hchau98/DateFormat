885d67e1704ba2a466ca929fba832a5db0e0ac49
hchau98 <chauhenry02@gmail.com>
2020-04-15 11:11:27 -0400

    Second Commit

--- src/DateFormat.java
+++ src/DateFormat.java
@@ -10,48 +10,97 @@ public class DateFormat {
         String day = date2[1];
         String date3 = date2[0];
         String month_string = "";
-        switch (date3) {
-            case "1":
-                month_string = "January";
+        int daysInt = Integer.parseInt(day);
+        int yearInt = Integer.parseInt(year);
+        int monthInt = Integer.parseInt(date3);
+        int numDays = 0;
+        switch (monthInt) {
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                numDays = 31;
+                if (daysInt>numDays) {
+                    System.out.println("Invalid Day");
+                    return;
+                }
                 break;
-            case "2":
-                month_string = "February";
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                numDays = 30;
+                if (daysInt>numDays) {
+                    System.out.println("Invalid Day");
+                    return;
+                }
                 break;
-            case "3":
-                month_string = "March";
-                break;
-            case "4":
-                month_string = "April";
-                break;
-            case "5":
-                month_string = "May";
-                break;
-            case "6":
-                month_string = "June";
-                break;
-            case "7":
-                month_string = "July";
-                break;
-            case "8":
-                month_string = "August";
-                break;
-            case "9":
-                month_string = "September";
-                break;
-            case "10":
-                month_string = "October";
-                break;
-            case "11":
-                month_string = "November";
-                break;
-            case "12":
-                month_string = "December";
+            case 2:
+                if (((yearInt % 4 == 0) && !(yearInt % 100 == 0)) || (yearInt % 400 == 0)) {
+                    numDays = 29;
+                    if (daysInt > numDays) {
+                        System.out.println("Invalid Day");
+                        return;
+                    }
+                }
+                else {
+                    numDays = 28;
+                    if (daysInt > numDays) {
+                        System.out.println("Invalid Day");
+                        return;
+                    }
+                }
                 break;
             default:
                 System.out.println("Invalid Month");
                 return;
         }
-        System.out.println("It is " + month_string + " " + day + "," + " " + year);
+            switch (monthInt) {
+                case 1:
+                    month_string = "January";
+                    break;
+                case 2:
+                    month_string = "February";
+                    break;
+                case 3:
+                    month_string = "March";
+                    break;
+                case 4:
+                    month_string = "April";
+                    break;
+                case 5:
+                    month_string = "May";
+                    break;
+                case 6:
+                    month_string = "June";
+                    break;
+                case 7:
+                    month_string = "July";
+                    break;
+                case 8:
+                    month_string = "August";
+                    break;
+                case 9:
+                    month_string = "September";
+                    break;
+                case 10:
+                    month_string = "October";
+                    break;
+                case 11:
+                    month_string = "November";
+                    break;
+                case 12:
+                    month_string = "December";
+                    break;
+                default:
+                    System.out.println("Invalid Month");
+                    return;
+            }
+            System.out.println("It is " + month_string + " " + day + "," + " " + year);
 
+        }
     }
-}
+
